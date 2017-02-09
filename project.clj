7c2f448bc02b5aa719d15cdac523b3c11b0456d9
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2017-02-09 16:39:47 -0600

    updated default portions of project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cbord-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject cbord-api "1.0.0"
+  :description "Provides access to the CBORD Get dining services web application through JSON for easier development of scripts and apps."
+  :url "http://github.com/PsychicNoodles/cbord-api"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [enlive "1.1.6"]
                  [clj-http "2.3.0"]
@@ -12,7 +12,7 @@
                  [ring/ring-defaults "0.2.1"]
                  [ring/ring-json "0.4.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [ring-logger "0.7.7"],,]
+                 [ring-logger "0.7.7"]]
   :main ^:skip-aot cbord-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
